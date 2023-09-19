ad825a7d4b7585ac8a1140a4eee1283fa0111e66
ricardo2silva <ricardojonasdasilva60@gmail.com>
2023-09-19 18:07:20 -0300

    criando seletores de formas diferentes

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  // seletor normal
+  //selector: 'app-servers',
+  
+  // 1- seletor como atributo
+  //selector: '[app-servers]',
+
+  // 2- seletor como classe
+  selector: '.app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
